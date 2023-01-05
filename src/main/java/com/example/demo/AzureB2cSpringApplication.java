92b31ef9d390aa851a1cb5a6205e45933b64e823
NIKHIL JADAV <nikhil.jadav@teknopoint.in>
2023-01-05 18:07:36 +0530

    first commit

---
+++ src/main/java/com/example/demo/AzureB2cSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureB2cSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureB2cSpringApplication.class, args);
+	}
+
+}
